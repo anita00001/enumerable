3db892f4c2bc25c4a8172cfb82551768c821ad2a
othniel2471 <othnielomaidu@gmail.com>
2023-08-24 13:45:05 +0100

    added tests and created lists

---
+++ main.rb
@@ -0,0 +1,18 @@
+require_relative  'my_enumerable'
+class MyList
+  include MyEnumerable
+
+  def initialize(*items)
+    @list = items
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+# list.my_filter {|e| e.even?}
+
+# list.my_any? {|e| e == 2}
+
+# list.my_any? {|e| e == 5}
